4f8d2d57971b3763fe2f78b1603d53436ff4040c
AlexandreOttmann <ottmann.alex@gmail.com>
2023-01-10 00:59:26 +0100

    Added animation on click and content

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 let reduceButton = document.querySelector('.reduce-button')
 
 let toggle = () =>{
-    let windowReduce = document.querySelector('.reduce')
-    let windowExpand = document.querySelector('.reduce-reverse')
     if(document.querySelector('.window').className == "window reduce"){
         document.querySelector('.window').className = "window reduce-reverse"
-  
         }
         else{
             document.querySelector('.window').className = "window reduce"
@@ -13,3 +10,44 @@ let toggle = () =>{
     
         }
     }
+
+
+
+    let expandButton = document.querySelector('.expand-button')
+
+    let toggle2 = () =>{
+
+
+        if(document.querySelector('.window').className == "window expand"){
+            document.querySelector('.window').className = "window expand-reverse"
+            document.querySelector('.container-window').className = "expension"
+      
+            }
+            else{
+                document.querySelector('.window').className = "window expand"
+                document.querySelector('.expension').className = "container-window"
+         
+        
+            }
+        }
+let body = document.querySelector('main')
+let closeButton = () =>{
+        document.querySelector('.container-window').style.display="none"
+        
+        setTimeout(()=>{
+            const button = body.appendChild(document.createElement('button'));
+            button.innerText = 'Bah alors ?'
+            button.classList.add('voidButton')
+            button.setAttribute('onclick', 'reset()')
+            setTimeout(()=>{
+                body.querySelector('.voidButton').style.display="block"
+                button.classList.add('active');
+            }, 1000)
+        }, 1000)
+}
+
+let reset = () => {
+    body.querySelector('.voidButton').remove()
+    document.querySelector('.container-window').style.display="flex"
+    
+}
